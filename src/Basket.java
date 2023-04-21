247197ea662c7d9ee6ff655155d3888052adbf6d
Yuri Dimitrenko <d89056578550@gmail.com>
2023-04-22 01:09:34 +0300

    22.04.23 commit

--- src/Basket.java
+++ src/Basket.java
@@ -5,75 +5,63 @@ import java.io.*;
 public class Basket {
     protected int[] prices;
     protected String[] products;
-    protected int[] basket;
+    private int[] amountOfProducts;
 
-    public Basket(int[] prices, String[] products, File file) {
-        this.prices = prices;
+    public Basket(String[] products, int[] prices) {
         this.products = products;
-        this.basket = new int[prices.length];
-        if (file.exists()) {
-            String[] text = loadFromTxtFile(file).split(" ");
-            int y = 0;
-            for (int i = 0; i < text.length - 1; i++) {
-                if (i % 2 != 0) {
-                    this.basket[y] = Integer.parseInt(text[i]);
-                    y++;
-                }
-            }
-            printCart();
-        }
-    }
-
-
-    public void showProducts() {
-        System.out.println("Список возможных товаров для покупки");
-        for (int i = 0; i < this.products.length; i++) {
-            System.out.println(this.products[i] + " " + this.prices[i] + " руб/шт.");
-        }
-    }
-
-    public int getLength() {
-        return this.basket.length;
+        this.prices = prices;
+        this.amountOfProducts = new int[products.length];
     }
 
     public void addToCart(int productNum, int amount) {
-        this.basket[productNum] += amount;
-    }
-
-    public void saveTxt(File file) throws Exception {
-        StringBuilder text = new StringBuilder();
-        for (int i = 0; i < this.basket.length; i++) {
-            text.append(i).append(" ").append(basket[i]).append(" ");
-        }
-        PrintWriter out = new PrintWriter(file);
-        out.println(text);
-        out.close();
-        file.createNewFile();
-    }
-
-    static String loadFromTxtFile(File textFile) {
-        StringBuilder text = new StringBuilder();
-        try (Reader reader = new FileReader(textFile)) {
-            int t;
-            while ((t = reader.read()) != -1) {
-                text.append((char) t);
-            }
-        } catch (IOException e) {
-            System.out.println(e.getMessage());
-        }
-        return text.toString();
+        amountOfProducts[productNum] += amount;
     }
 
     public void printCart() {
         int sum = 0;
         System.out.println("Ваша корзина:");
-        for (int i = 0; i < basket.length; i++) {
-            if (basket[i] != 0) {
-                System.out.println(products[i] + " " + basket[i] + " шт. " + prices[i] + " руб/шт " +
-                        (prices[i] * basket[i]) + " руб. в сумме");
-                sum += prices[i] * basket[i];
+        for (int i = 0; i < amountOfProducts.length; i++) {
+            if (amountOfProducts[i] > 0) {
+                System.out.println(products[i] + " - " + amountOfProducts[i] + " шт., " + prices[i] + " руб. за шт., " + amountOfProducts[i] * prices[i] + " руб. в сумме");
+                sum += amountOfProducts[i] * prices[i];
             }
         }
-        System.out.println("Итого: " + sum + " руб.");
+        System.out.println("Итого " + sum + " руб");
+    }
+
+    public void saveTxt(File textFile) throws IOException {
+        try (FileWriter file = new FileWriter(textFile);
+             PrintWriter out = new PrintWriter(file)) {
+            for (String product : products) {
+                out.print(product + " ");
+            }
+            out.println();
+            for (int price : prices) {
+                out.print(price + " ");
+            }
+            out.println();
+            for (int amount : amountOfProducts) {
+                out.print(amount + " ");
+            }
+        }
+    }
+
+    public static Basket loadFromTxtFile(File textFile) throws IOException {
+        try (BufferedReader in = new BufferedReader(new FileReader(textFile))) {
+            String[] products = in.readLine().split(" ");
+            String[] prices = in.readLine().split(" ");
+            int[] convertedPrices = new int[prices.length];
+            for (int i = 0; i < prices.length; i++) {
+                convertedPrices[i] = Integer.parseInt(prices[i]);
+            }
+            String[] amount = in.readLine().split(" ");
+            int[] convertedAmount = new int[amount.length];
+            for (int ii = 0; ii < amount.length; ii++) {
+                convertedAmount[ii] = Integer.parseInt(amount[ii]);
+            }
+            Basket result = new Basket(products, convertedPrices);
+            result.amountOfProducts = convertedAmount;
+            return result;
+        }
     }
 }
